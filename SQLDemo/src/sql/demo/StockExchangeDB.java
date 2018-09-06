9abd724746babea6322427de2274d122c48d745b
MaximKrasikov <karasy77@yandex.ru>
2018-09-06 08:06:39 +0300

    Added h2BeginWork

--- SQLDemo/src/sql/demo/StockExchangeDB.java
+++ SQLDemo/src/sql/demo/StockExchangeDB.java
@@ -1,24 +1,41 @@
 package sql.demo;
 
+import com.sun.rmi.rmid.ExecOptionPermission;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.sql.*;
-public class StockExchangeDB {
-    // Блок объявления констант
-    public static final String DB_URL = "jdbc:h2:/c:/JavaPrj/SQLDemo/db/stockExchange";
-    public static final String DB_Driver = "org.h2.Driver";
 
-    public static void main(String[] args) {
-        try {
-            Class.forName(DB_Driver); //Проверяем наличие JDBC драйвера для работы с БД
-            Connection connection = DriverManager.getConnection(DB_URL);//соединениесБД
-            System.out.println("Соединение с СУБД выполнено.");
-            connection.close();       // отключение от БД
-            System.out.println("Отключение от СУБД выполнено.");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace(); // обработка ошибки  Class.forName
-            System.out.println("JDBC драйвер для СУБД не найден!");
-        } catch (SQLException e) {
-            e.printStackTrace(); // обработка ошибок  DriverManager.getConnection
-            System.out.println("Ошибка SQL !");
+public class StockExchangeDB {
+    public static void main(String[] args) throws Exception {
+
+        Class.forName("org.h2.Driver").newInstance();
+        Connection conn = DriverManager.getConnection("jdbc:h2:/c:/JavaPrj/SQLDemo/db/stockExchange");
+
+        Statement st = conn.createStatement();
+        st.execute("DROP TABLE STUDENTS IF EXISTS");//удалить таблицу если есть
+        st.execute("CREATE TABLE STUDENTS(ID INT PRIMARY KEY, NAME VARCHAR(255),  HOBBY VARCHAR(255));");// создать таблицу со столбцами
+        st.execute("INSERT INTO STUDENTS VALUES(1, 'Ivanov', '');");// помещаем значения
+        st.execute("INSERT INTO STUDENTS VALUES(2, 'Petrov', 'Swim');");
+        st.execute("INSERT INTO STUDENTS VALUES(3, 'Sidorov', '');");
+        st.execute("INSERT INTO STUDENTS VALUES(4, 'Andreev', 'Wrestling');");
+
+        ResultSet result = st.executeQuery("SELECT * FROM STUDENTS");// формируем запрос
+
+        String id = "", name = "", hobby = "";
+
+        while (result.next()) {
+            // формирование переменных
+            id = result.getString("ID");
+            name = result.getString("NAME");
+            hobby = result.getString("HOBBY");
+
+
+            if (hobby.equals("")) {
+                hobby = "Нет хобби";
+            }
+            System.out.printf("%3s | %-10s | %-10s \n", id, name, hobby);
         }
     }
 }
